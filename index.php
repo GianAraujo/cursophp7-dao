6a5180f7c6e6c0310962542134a62f8e44fb205f
Gian Araujo <gan.araujo@gmail.com>
2020-04-23 17:07:41 -0300

    criado a clesse usuarios

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 require_once("config.php");
 
-$sql = new Sql();
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+#$sql = new Sql();
+#$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+#echo json_encode($usuarios);
 
-echo json_encode($usuarios);
+$user= new Usuario();
+$user->loadById(7);
+echo $user;
 
 ?>
